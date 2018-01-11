95e1734988d4248442041a6658d5c5fd44702c7d
bennybop <bennybop74@yahoo.com>
2018-01-11 22:47:54 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,15 +9,23 @@ class Artist
   end
 
   def add_song(song)
-    @songs << song
-    song.artist = self
+    self.songs << song 
+    song.artist = self 
+    @@song_count +=1 
   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song 
+    song.artist = self 
+    @@song_count += 1
+  end 
 
   def songs
     @songs
   end
 
-  def genres
-    self.songs.collect {|song| song.genre}
-  end
+  def self.song_count 
+    @@song_count 
+  end 
 end
