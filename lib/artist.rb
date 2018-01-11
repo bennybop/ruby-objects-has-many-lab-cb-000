6f582b239f7079f7b40150fcfda3770980b46e40
bennybop <bennybop74@yahoo.com>
2018-01-11 22:39:56 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,5 +1,5 @@
 class Artist
-  attr_accessor :name, :songs 
+  attr_accessor :name, :songs
 
   @@song_count = 0
 
@@ -8,7 +8,7 @@ class Artist
     @songs = []
   end
 
-def add_song(song)
+def add_song(song, artist)
   self.songs << song
   song.artist = self
 end
@@ -21,5 +21,3 @@ def self.song_count
   @@song_count
 end
 end
-
-
